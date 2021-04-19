c06a2b95b867484f06b752c022e50b629fcc3d28
Alessandra Mitie <alessandra.mitie@gmail.com>
2021-04-18 23:23:23 -0300

    criacao de um objeto literal

--- src/routes.js
+++ src/routes.js
@@ -15,87 +15,95 @@ const profile ={
     "value-hour": 75
 }
 
-const jobs = [
-    {
-        id: 1,
-        name: "Pizzaria Guloso",
-        "daily-hours": 2,
-        "total-hours": 60,
-        created_at: Date.now()
-    },
-    {
-        id: 2,
-        name: "OneTwo Project",
-        "daily-hours": 3,
-        "total-hours": 47,
-        created_at: Date.now()
-    }
-]
+//objeto literal
+const Job = {
+    data: [
+        {
+            id: 1,
+            name: "Pizzaria Guloso",
+            "daily-hours": 2,
+            "total-hours": 1,
+            created_at: Date.now()
+        },
+        {
+            id: 2,
+            name: "OneTwo Project",
+            "daily-hours": 3,
+            "total-hours": 47,
+            created_at: Date.now()
+        }
+    ],
+    //objeto para controlar
+    controllers: {
+        index(req, res) {
+            const updatedJobs = Job.data.map((job) => {
+            //map é usado para poder retornar algo. Com o forEach não seria possível
+            
+                const remaining = Job.services.remainingDays(job)
+                const status = remaining <=0 ? 'done' : 'progress'
+            
+                return {
+                //espalhamento(pegar tudo o que tem no objeto (no caso job) e espalhar no novo objeto)
+                    ...job,
+                    remaining,
+                    status,
+                    budget: profile["value-hour"] * job["total-hours"]
+                }
+            })
+            
+            res.render(views + "index", { jobs: updatedJobs })
+        },
 
-function remainingDays (job) {
-    //ajustes no job (cálculo de tempo restante)
-    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+        create(req, res) {
+            return res.render(views + "job")
+        },
+
+        save(req, res) {
+            //req.body = {name: 'asdf', 'daily-hours': '3', 'total-hours': '30'}
+         
+            const lastId = Job.data[Job.data.length - 1]?.id || 1;
+            //se a condição achar o objeto no array, então vai pegar o id dele e atribuir em lastId
+            // || significa ou
+            //se a condição não achar, então vai ser o número 1
         
-    const createdDate = new Date(job.created_at)
-    const dueDay = createdDate.getDate() + Number(remainingDays)
-    //precisa passar o Number pois o toFixed transforma em string
-    //data futura de vencimento em milissegundos:
-    const dueDateInMs = createdDate.setDate(dueDay)
-    //diferença do tempo em milissegundos
-    const timeDiffInMs = dueDateInMs - Date.now()
-    //transformar milissegundos em dias
-    const dayInMs = 1000 * 60 * 60 * 24
-    //difereça de dias que faltam
-    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
-    //restam x dias:
-    return dayDiff
+            jobs.push({
+                id: lastId + 1,
+                name: req.body.name,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"],
+                created_at: Date.now() //atribuindo uma nova data a partir da criação
+            })
+            return res.redirect('/')
+        }
+    },
+    services: {
+        remainingDays (job) {
+            //ajustes no job (cálculo de tempo restante)
+            const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+                
+            const createdDate = new Date(job.created_at)
+            const dueDay = createdDate.getDate() + Number(remainingDays)
+            //precisa passar o Number pois o toFixed transforma em string
+            //data futura de vencimento em milissegundos:
+            const dueDateInMs = createdDate.setDate(dueDay)
+            //diferença do tempo em milissegundos
+            const timeDiffInMs = dueDateInMs - Date.now()
+            //transformar milissegundos em dias
+            const dayInMs = 1000 * 60 * 60 * 24
+            //difereça de dias que faltam
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+            //restam x dias:
+            return dayDiff
+        }
+    }
 }
-
 
 //render é uma função do ejs que entende os caminhos de rotas
 //para pegar as rotas:
-routes.get('/', (req, res) => {
-
-    const updateJobs = jobs.map((job) => {
-    //map é usado para poder retornar algo. Com o forEach não seria possível
-
-        const remaining = remainingDays(job)
-        const status = remaining <=0 ? 'done' : 'progress'
-
-        return {
-            //espalhamento(pegar tudo o que tem no objeto (no caso job) e espalhar no novo objeto)
-            ...job,
-            remaining,
-            status,
-            budget: profile["value-hour"] * job["total-hours"]
-        }
-    })
-
-    
-    
-    res.render(views + "index", { jobs: updateJobs })
-
-
-})
-routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/', Job.controllers.index)
+routes.get('/job', Job.controllers.create)
 //pegar os dados na requisição
-routes.post('/job', (req, res) => {
-    //req.body = {name: 'asdf', 'daily-hours': '3', 'total-hours': '30'}
- 
-    const lastId = jobs[jobs.length - 1]?.id || 1;
-    //se a condição achar o objeto no array, então vai pegar o id dele e atribuir em lastId
-    // || significa ou
-    //se a condição não achar, então vai ser o número 1
-
-    jobs.push({
-        id: lastId + 1,
-        name: req.body.name,
-        "daily-hours": req.body["daily-hours"],
-        "total-hours": req.body["total-hours"],
-        created_at: Date.now() //atribuindo uma nova data a partir da criação
-    })
-    return res.redirect('/')
-})
+routes.post('/job', Job.controllers.save)
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 //vai enviar o objeto profile
