3bb377ce36c6e9ace2ff2bafd1a9612a2f73260e
alessandramitie <alessandra.mitie@gmail.com>
2021-05-12 23:52:01 -0300

    criacao de novo job passando no banco de dados

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -7,17 +7,16 @@ module.exports = {
         return res.render("job")
     },
 
-    save(req, res) {
+    async save(req, res) {
         //req.body = {name: 'asdf', 'daily-hours': '3', 'total-hours': '30'}
      
-        const jobs = Job.get()
-        const lastId = jobs[jobs.length - 1]?.id || 0;
+        //const jobs = await Job.get()
+        //const lastId = jobs[jobs.length - 1]?.id || 0;
         //se a condição achar o objeto no array, então vai pegar o id dele e atribuir em lastId
         // || significa ou
         //se a condição não achar, então vai ser o número 1
     
-        Job.create({
-            id: lastId + 1,
+        await Job.create({
             name: req.body.name,
             "daily-hours": req.body["daily-hours"],
             "total-hours": req.body["total-hours"],
@@ -27,10 +26,10 @@ module.exports = {
         return res.redirect('/')
     },
 
-    show(req, res) {
+    async show(req, res) {
 
         const jobId = req.params.id
-        const jobs = Job.get()
+        const jobs = await Job.get()
 
         //buscar dentro do array: para cada um dos dados vai rodar uma função e se for o valor, vai atribuir na const
         const job = jobs.find(job => Number(job.id) === Number(jobId))
@@ -40,16 +39,16 @@ module.exports = {
             return res.send('Job not found!')
         }
 
-        const profile = Profile.get()
+        const profile = await Profile.get()
 
         job.budget = JobUtils.calculateBudget(job, profile["value-hour"])
 
         return res.render("job-edit", { job })
     },
 
-    update(req, res) {
+    async update(req, res) {
         const jobId = req.params.id
-        const jobs = Job.get()
+        const jobs = await Job.get()
 
         //buscar dentro do array: para cada um dos dados vai rodar uma função e se for o valor, vai atribuir na const
         const job = jobs.find(job => Number(job.id) === Number(jobId))
